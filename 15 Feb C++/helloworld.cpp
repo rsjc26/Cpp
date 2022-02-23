eeabba6da5e0361da8505182eee117370c27b259
rsjc26 <98102222+rsjc26@users.noreply.github.com>
2022-02-23 16:16:42 +0530

    Add files via upload

---
+++ 15 Feb C++/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int i;
+cout << "This is Output.\n";
+cout << "Enter a number;";
+cin >> i;
+cout << i << "squared is " << i*i<<"\n";
+return 0;
+}
